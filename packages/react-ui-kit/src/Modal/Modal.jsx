f3cfb3661df43a68bd4954885133622205b4584e
Michael Koppen <Yserz@users.noreply.github.com>
2018-08-23 14:56:09 +0200

    Revert "chore: Modal data-uie (#1040)"
    This reverts commit 53a6f99dcdd7fa4a7b671ef536547e6d05aa1470.

--- packages/react-ui-kit/src/Modal/Modal.jsx
+++ packages/react-ui-kit/src/Modal/Modal.jsx
@@ -84,7 +84,7 @@ const ModalBackground = styled(OverlayBackground)`
 const noop = () => {};
 
 const Modal = ({children, fullscreen, onClose, onBackgroundClick, ...props}) => (
-  <OverlayWrapper {...props} data-uie-name="modal">
+  <OverlayWrapper {...props}>
     <ModalBody fullscreen={fullscreen}>
       <ModalContent>{children}</ModalContent>
       {onClose !== noop && <ModalClose onClick={onClose} data-uie-name="do-close" />}
