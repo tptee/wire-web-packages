d55680b92c72a39cf2c46436713123d6fe497c03
Florian Keller <florian.keller@wire.com>
2018-09-28 18:03:02 +0200

    Add travis status bot

---
+++ packages/travis-status-bot/README.md
@@ -0,0 +1,41 @@
+# Wire
+
+This repository is part of the source code of Wire. You can find more information at [wire.com](https://wire.com) or by contacting opensource@wire.com.
+
+You can find the published source code at [github.com/wireapp](https://github.com/wireapp).
+
+For licensing information, see the attached LICENSE file and the list of third-party licenses at [wire.com/legal/licenses/](https://wire.com/legal/licenses/).
+
+## Changelog Bot
+
+A bot listening to the RSS feed on [traviscistatus.com](https://www.traviscistatus.com) and reporting to a given chat.
+
+### Getting Started
+
+```
+yarn
+yarn start
+```
+
+### Installation
+
+```
+yarn global add @wireapp/travis-status-bot
+```
+
+### Usage
+
+Just ask for `--help` to get informed on how to use this bot:
+
+**Bash**
+
+```bash
+travis-status-bot --help
+```
+
+**Node**
+
+```bash
+yarn dist
+node dist/cli.js --help
+```
---
+++ packages/travis-status-bot/src/TravisStatusBot.ts
@@ -0,0 +1,102 @@
+/*
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+//import * as path from 'path';
+//import * as logdown from 'logdown';
+//import {FileEngine} from '@wireapp/store-engine';
+import {MessageHandler} from '@wireapp/bot-api';
+import {PayloadBundleIncoming, PayloadBundleType, ReactionType} from '@wireapp/core/dist/conversation/root';
+
+import {Connection, ConnectionStatus} from '@wireapp/api-client/dist/commonjs/connection';
+import {TextContent} from '@wireapp/core/dist/conversation/content';
+import {TravisRSSFeed} from './TravisRSSFeed';
+
+const {version}: {version: string} = require('../package.json');
+
+class MainHandler extends MessageHandler {
+  //private readonly logger: logdown.Logger;
+  //private readonly STORE_PATH = path.resolve(__dirname, '.temp');
+  //private readonly storeEngine: FileEngine;
+  private readonly travisRSSFeed: TravisRSSFeed;
+  private readonly helpText = `**Hello!** 😎 This is Travis Status Bot v${version} speaking.\n\nAvailable commands:\n- /feed \n- /help\n- /subscribe\n`;
+
+  constructor() {
+    super();
+    //this.storeEngine = new FileEngine(this.STORE_PATH);
+    this.travisRSSFeed = new TravisRSSFeed();
+    /*this.logger = logdown('@wireapp/travis-status-bot/MainHandler', {
+      logger: console,
+      markdown: false,
+    });*/
+    console.log(this.helpText);
+  }
+
+  public handleEvent(payload: PayloadBundleIncoming) {
+    switch (payload.type) {
+      case PayloadBundleType.TEXT: {
+        if (payload.conversation) {
+          const messageContent = payload.content as TextContent;
+          return this.handleText(payload.conversation, messageContent.text, payload.id, payload.from);
+        }
+      }
+      case PayloadBundleType.CONNECTION_REQUEST: {
+        const connectRequest = payload.content as Connection;
+        if (payload.conversation && connectRequest.status !== ConnectionStatus.CANCELLED) {
+          return this.handleConnectionRequest(connectRequest.to, payload.conversation);
+        }
+      }
+      default:
+        return;
+    }
+  }
+
+  public async handleText(conversationId: string, text: string, messageId: string, senderId: string): Promise<void> {
+    switch (text) {
+      case '/feed': {
+        await this.sendReaction(conversationId, messageId, ReactionType.LIKE);
+        const feed = await this.travisRSSFeed.getFeed();
+        let response = '';
+        if (feed.length) {
+          response = feed.reduce(
+            (result, item) => (result += `"${item.title}" (${item.date ? item.date.toString() : ''})`),
+            ''
+          );
+        } else {
+          response = 'No items found :(';
+        }
+        return this.sendText(conversationId, response);
+      }
+      case '/help': {
+        await this.sendReaction(conversationId, messageId, ReactionType.LIKE);
+        return this.sendText(conversationId, this.helpText);
+      }
+      case '/subscribe': {
+        await this.sendReaction(conversationId, messageId, ReactionType.LIKE);
+        return this.sendText(conversationId, 'Not yet implemented :(');
+      }
+    }
+  }
+
+  async handleConnectionRequest(userId: string, conversationId: string): Promise<void> {
+    await this.sendConnectionResponse(userId, true);
+    await this.sendText(conversationId, this.helpText);
+  }
+}
+
+export {MainHandler};
